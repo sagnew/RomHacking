c579148a0e5da18d9e531acdfd00433863778df0
Sam Agnew <sagnew@eden.rutgers.edu>
2015-01-18 06:21:48 -0500

    Implement basic memory editing functions for super mario bros

--- test.lua
+++ test.lua
@@ -3,10 +3,27 @@ old_button = "";
 count = 0;
 fact = "";
 
+function kill_mario ()
+  -- Sets the timer to zero.
+  memory.writebyte(0x07F8, 0);
+  memory.writebyte(0x07F9, 0);
+  memory.writebyte(0x07FA, 0);
+end;
+
+function read_coins ()
+  -- Returns the number of coins the user has
+  return memory.readbyte(0x07ED) .. memory.readbyte(0x07EE)
+end;
+
+function edit_coins (left, right)
+  memory.writebyte(0x07ED, left);
+  memory.writebyte(0x07EE, right);
+end;
+
 while (true) do
   for key, value in pairs(joypad.get(1)) do
     if(value) then
-    	button = key;
+      button = key;
     end;
   end;
 
@@ -17,6 +34,7 @@ while (true) do
     io.output(output);
     io.write(count);
     io.close(output);
+    kill_mario();
   end;
 
   input = io.open("input.txt", "r");
@@ -25,10 +43,10 @@ while (true) do
   io.close(input);
 
   if(input_content ~= nil) then
-  	fact = input_content
+    fact = input_content
   end;
 
   old_button = button;
-  gui.text(50, 100, fact);
+  gui.text(0, 50, fact);
   emu.frameadvance();
 end;
