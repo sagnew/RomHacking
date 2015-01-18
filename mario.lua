b985180f83107a38e82a3a050d41870dc3aa268e
Sam Agnew <sagnew@eden.rutgers.edu>
2015-01-18 09:13:57 -0500

    Implement postmates API timing

--- mario.lua
+++ mario.lua
@@ -4,49 +4,73 @@ count = 0;
 fact = "";
 
 function kill_mario ()
-  -- Sets the timer to zero.
-  memory.writebyte(0x07F8, 0);
-  memory.writebyte(0x07F9, 0);
-  memory.writebyte(0x07FA, 0);
+    -- Sets the timer to zero.
+    memory.writebyte(0x07F8, 0);
+    memory.writebyte(0x07F9, 0);
+    memory.writebyte(0x07FA, 0);
 end;
 
 function read_coins ()
-  -- Returns the number of coins the user has
-  return memory.readbyte(0x07ED) .. memory.readbyte(0x07EE)
+    -- Returns the number of coins the user has
+    return memory.readbyte(0x07ED) .. memory.readbyte(0x07EE)
 end;
 
 function edit_coins (left, right)
-  memory.writebyte(0x07ED, left);
-  memory.writebyte(0x07EE, right);
+    memory.writebyte(0x07ED, left);
+    memory.writebyte(0x07EE, right);
+end;
+
+function edit_score (time)
+    address = 0x07E3;
+    numstr = "";
+    time = time - 200;
+    while (address >= 0x07DD) do
+    	if time > 1 then
+            memory.writebyte(address, time % 10);
+            time = time / 10;
+        else
+            memory.writebyte(address, 0);
+        end;
+        address = address - 1;
+    end;
+end;
+
+function write_file (filename, text)
+    output = io.open(filename, "w");
+    io.output(output);
+    io.write(text);
+    io.close(output);
+end;
+
+function read_file (filename)
+    input = io.open(filename, "r");
+    io.input(input);
+    input_content = io.read();
+    io.close(input);
+
+    return input_content;
 end;
 
 while (true) do
-  for key, value in pairs(joypad.get(1)) do
-    if(value) then
-      button = key;
+    for key, value in pairs(joypad.get(1)) do
+        if(value) then
+            button = key;
+        end;
     end;
-  end;
 
-  if(button == "select" and old_button ~= button) then
-    count = count + 1;
+    if(button == "select" and old_button ~= button) then
+        count = count + 1;
+        write_file("output.txt", count);
+    end;
 
-    output = io.open("output.txt", "w");
-    io.output(output);
-    io.write(count);
-    io.close(output);
-    kill_mario();
-  end;
+    input_content = read_file("input.txt");
+    if(input_content ~= nil) then
+        fact = input_content
+    end;
 
-  input = io.open("input.txt", "r");
-  io.input(input);
-  input_content = io.read();
-  io.close(input);
+    edit_score(tonumber(read_file("time.txt")));
 
-  if(input_content ~= nil) then
-    fact = input_content
-  end;
-
-  old_button = button;
-  gui.text(0, 50, fact);
-  emu.frameadvance();
+    old_button = button;
+    gui.text(0, 50, memory.readbyte(0x07DF));
+    emu.frameadvance();
 end;
--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import time
+
+import json
 import requests
+import arrow
 
 old_count = 0
 while True:
+    previous_text = ''
+    with open('input.txt', 'r') as input_text:
+        previous_text = input_text.read()
     with open('input.txt', 'w') as input_content, open('output.txt', 'r') as output_content:
         count = int(output_content.read())
         if count > old_count:
@@ -10,4 +16,16 @@ while True:
             print fact
             input_content.write(fact)
             old_count = count
-        time.sleep(1)
+        else:
+            input_content.write(previous_text)
+    delivery_json = str(requests.get('https://api.postmates.com/v1/customers/cus_JyoSxlnoD8cMyk/deliveries', auth=('3d0dd751-ab19-460d-95a0-436e279ca72b', '')).text)
+    delivery = json.loads(delivery_json)
+    with open('time.txt', 'w') as time_file:
+        now = arrow.utcnow()
+        eta = arrow.get(delivery['data'][0]['pickup_eta'])
+        diff = eta - now
+        print eta.humanize()
+        print now
+        print diff.seconds
+        time_file.write(str(diff.seconds))
+    time.sleep(1)
